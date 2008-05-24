0db616bba953eab5afa570d0468f4613809dbee4
Ian Esling <ian@BayStreet.(none)>
2008-05-24 22:48:07 +0100

    refactoring

--- grails-app/controllers/SquidController.groovy
+++ grails-app/controllers/SquidController.groovy
@@ -1,14 +1,24 @@
 class SquidController {
 
+    def orderStatus
+
     def index = { }
 
     def order = {OrderForm form ->
-        def turn = new Turn(player:form.player, turnNumber:form.turnNumber, moveTo:form.moveTo)
-        turn.save(flush:true)
+        def turn = new Turn(player: form.player, turnNumber: form.turnNumber, moveTo: form.moveTo)
+        turn.save(flush: true)
+        orderStatus(form)
     }
 
-    Integer lastTurnForPlayer(String player)
-    {
+    private String orderStatus(OrderForm form) {
+        orderStatus = (lastTurnForPlayer(otherPlayer(form.player)) == form.turnNumber) ? "resolved" : "waiting"
+    }
+
+    String otherPlayer(String player) {
+        player.equals("A") ? "B" : "A"
+    }
+
+    Integer lastTurnForPlayer(String player) {
         def i = 0
         Turn.list().each {Turn turn ->
             if (turn.player.equals(player) && turn.turnNumber > i)
@@ -17,13 +27,11 @@ class SquidController {
         return i
     }
 
-    String playerAPosition()
-    {
+    String playerAPosition() {
         Turn.findByPlayerAndTurnNumber("A", lastTurnForPlayer("A")).moveTo
     }
 
-    String playerBPosition()
-    {
+    String playerBPosition() {
         Turn.findByPlayerAndTurnNumber("B", lastTurnForPlayer("B")).moveTo
     }
 }
--- test/integration/SquidControllerTests.groovy
+++ test/integration/SquidControllerTests.groovy
@@ -44,4 +44,28 @@ class SquidControllerTests extends GroovyTestCase {
         assertEquals("Player A position wrong after Player B turn", squid.playerAPosition(), "B2")
         assertEquals("PLayer B position wrong", squid.playerBPosition(), "Z99")
     }
+
+    void testOrderResolution() {
+        def squid = new SquidController()
+        squid.params.moveTo = "A1"
+        squid.params.turnNumber = 1
+        squid.params.player = "A"
+        squid.order.call()
+        assertEquals("orders resolved after only player A moved", "waiting", squid.orderStatus)
+        squid.params.moveTo = "Z1"
+        squid.params.turnNumber = 1
+        squid.params.player = "B"
+        squid.order.call()
+        assertEquals("orders not resolved after player B moved", "resolved", squid.orderStatus)
+        squid.params.moveTo = "Z2"
+        squid.params.turnNumber = 2
+        squid.params.player = "B"
+        squid.order.call()
+        assertEquals("orders resolved after player B moved turn 2", "waiting", squid.orderStatus)
+        squid.params.moveTo = "A2"
+        squid.params.turnNumber = 2
+        squid.params.player = "A"
+        squid.order.call()
+        assertEquals("orders not resolved after player A moved turn 2", "resolved", squid.orderStatus)
+    }
 }
